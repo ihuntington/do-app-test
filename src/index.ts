fc525aa729ae2cc729cec6d26e3294e6bfae5080
Ian Huntington <ian.huntington@plumguide.com>
2022-10-12 11:21:17 +0100

    Add simple Fastify server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello world 2")
+import Fastify from "fastify";
+
+const fastify = Fastify({
+    logger: true,
+})
+
+fastify.get("/", async () => {
+    return { hello: "world" }
+})
+
+async function start() {
+    try {
+        await fastify.listen({
+            host: "0.0.0.0",
+            port: 8080,
+        })
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+
+start()
